db48d965762524249533aee8e7e9e6e2a7147a8e
Harihshshyam <harishyamhari5555@gmail.com>
2025-11-09 13:31:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<?php echo "🚀 Auto Deploy Test Successful!"; ?>
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
